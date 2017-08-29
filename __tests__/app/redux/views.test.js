486b345c0a0ddd5512c7191bb69d04c54d9c8242
MacKentoch <erwan.datin@gmail.com>
2017-08-29 06:28:51 +0200

    tests

--- __tests__/app/redux/views.test.js
+++ __tests__/app/redux/views.test.js
@@ -1,6 +1,7 @@
 // @flow
 
 import views                from '../../../src/app/redux/modules/views';
+import * as viewsActions    from '../../../src/app/redux/modules/views';
 import moment               from 'moment';
 
 const dateFormat = 'DD/MM/YYYY HH:mm';
@@ -153,5 +154,89 @@ describe('redux - reducer "views"', () => {
       leaveTime:    now
     };
     expect(views({currentView: 'protected'}, actionLeaveComponents)).toEqual(expectedState);
+  });
+});
+
+describe('redux - action creator "views"', () => {
+  const  now = moment().format(dateFormat);
+
+  it('should return enterHome action object', () => {
+    const enterHomeACTION = {
+      type:         'ENTER_HOME_VIEW',
+      currentView:  'home',
+      enterTime:    now,
+      leaveTime:    null
+    };
+    expect(viewsActions.enterHome(now)).toEqual(enterHomeACTION);
+  });
+
+  it('should return leaveHome action object', () => {
+    const leaveHomeACTION = {
+      type:         'LEAVE_HOME_VIEW',
+      currentView:  'home',
+      enterTime:    null,
+      leaveTime:    now
+    };
+    expect(viewsActions.leaveHome(now)).toEqual(leaveHomeACTION);
+  });
+
+  it('should return enterAbout action object', () => {
+    const enterAboutACTION = {
+      type:         'ENTER_ABOUT_VIEW',
+      currentView:  'about',
+      enterTime:    now,
+      leaveTime:    null
+    };
+    expect(viewsActions.enterAbout(now)).toEqual(enterAboutACTION);
+  });
+
+  it('should return leaveAbout action object', () => {
+    const leaveAboutACTION = {
+      type:         'LEAVE_ABOUT_VIEW',
+      currentView:  'about',
+      enterTime:    null,
+      leaveTime:    now
+    };
+    expect(viewsActions.leaveAbout(now)).toEqual(leaveAboutACTION);
+  });
+
+  it('should return enterLogin action object', () => {
+    const enterComponentsACTION = {
+      type:         'ENTER_LOGIN_VIEW',
+      currentView:  'Login',
+      enterTime:    now,
+      leaveTime:    null
+    };
+    expect(viewsActions.enterLogin(now)).toEqual(enterComponentsACTION);
+  });
+
+  it('should return leaveLogin action object', () => {
+    const leaveComponentsACTION = {
+      type:         'LEAVE_LOGIN_VIEW',
+      currentView:  'Login',
+      enterTime:    null,
+      leaveTime:    now
+    };
+    expect(viewsActions.leaveLogin(now)).toEqual(leaveComponentsACTION);
+  });
+
+  it('should return enterProtected action object', () => {
+    const enterComponentsACTION = {
+      type:         'ENTER_PROTECTED_VIEW',
+      currentView:  'Protected',
+      enterTime:    now,
+      leaveTime:    null
+    };
+    expect(viewsActions.enterProtected(now)).toEqual(enterComponentsACTION);
+  });
+
+  it('should return leaveProtected action object', () => {
+    const leaveComponentsACTION = {
+      type:         'LEAVE_PROTECTED_VIEW',
+      currentView:  'Protected',
+      enterTime:    null,
+      leaveTime:    now
+    };
+    expect(viewsActions.leaveProtected(now)).toEqual(leaveComponentsACTION);
   });
 });
