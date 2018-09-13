22db6f185fb3cb2ca5d7c4619b3bd16bb165d8d0
MacKentoch <erwan.datin@gmail.com>
2018-09-13 05:50:56 +0200

    tests

---
+++ src/front/pages/about/__tests__/About.test.js
@@ -0,0 +1,24 @@
+// @flow
+
+// #region imports
+import React from 'react';
+import renderer from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import { MemoryRouter } from 'react-router';
+import About from '../About';
+// #endregion
+
+describe('About page', () => {
+  it('renders as expected', () => {
+    const props = {};
+    const component = renderer
+      .create(
+        <div>
+          <MemoryRouter>
+            <About {...props} />
+          </MemoryRouter>
+        </div>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
